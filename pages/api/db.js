6c1bacb1bbd8c58ece9ddc9410cb7bbc592718e7
Lewis Coulson <lewis.coulson@johnlewis.co.uk>
2020-06-12 15:20:30 +0100

    Try using mongodb atlas

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,6 +1,9 @@
 import connect from '../../lib/connect'
 
-connect('mongodb://localhost:27017/property-dashboard')
+connect(
+  'mongodb+srv://admin:propertydashboard@cluster0-qyav1.mongodb.net/property-dashboard?retryWrites=true&w=majority',
+  { useNewUrlParser: true }
+)
   .then(async () => {
     console.log('connected to mongodb')
   })
